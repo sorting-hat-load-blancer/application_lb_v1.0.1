93235e5daeba2414443295d7c0eea3c82b796335
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-19 23:09:08 +0530

    listen incomming conn and err handle

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use std::io;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn TcpServer() {
     // Handle the client connection
     let listner = TcpListener::bind("127.0.0.1:9000").unwrap();
+    println!("Application LoadBlancer Running on 127.0.0.1:9000");
+
+    for stream in listner.incoming() {
+        match stream {
+            Ok(mut stream) => {
+
+            }
+            Err(e) => {
+                eprintln!("Error Acepting Connection : {}", e)
+            }
+        }
+    }
 }
 fn main() {
     println!("Hello, world!");
