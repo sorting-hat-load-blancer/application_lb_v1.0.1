4c498b662b63777dcfaa3f8562793bcfefbdb204
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-19 23:01:58 +0530

    add tcplisner bind ip and port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+use std::net::{TcpListener, TcpStream};
+
+fn TcpServer() {
+    // Handle the client connection
+    let listner = TcpListener::bind("127.0.0.1:9000").unwrap();
+}
 fn main() {
     println!("Hello, world!");
 }
