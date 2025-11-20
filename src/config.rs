3d2a54ea7bd8780bbb94d7e17053f4616b4b19b1
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-20 23:11:59 +0530

    added some config field

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,14 @@
 
+// define server
+struct Server {
+    server_ip: String,
+    server_port: u16
+}
+
 // define health check
 struct HealthCheck {
-    
+    health_check_servers: Vec<Server>,
+    health_check_time: u32  // miliseconds
 }
 
 // configuration struct
@@ -9,5 +16,8 @@ struct Configuration {
     protocol: String,
     server_run_port: u16,
     upstream_servers: Vec<String>,
+    health_check: HealthCheck,
+    http_headers: bool,     // bool value -> http headers forward to upstream server (true -> forward header, false -> without forward)
+    ssl_support: bool,
     
 }
